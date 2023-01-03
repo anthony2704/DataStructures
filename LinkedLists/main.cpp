b6eeeb05d2500e27541c7307a4348347385ee8d0
anthony2704 <anthony2742000@gmail.com>
2023-01-03 08:58:20 +0700

    Add Remove(index)

--- LinkedLists/main.cpp
+++ LinkedLists/main.cpp
@@ -21,9 +21,11 @@ public:
         Node* next;
         
         public:
+            Node(): next(NULL) {}
             Node(T data = 0){
                 T data = 0;
-                this->next = NULL;
+                //this->next = NULL;
+                next(NULL);
             }
     };
 
@@ -79,4 +81,77 @@ public:
 
         count++;
     }
+
+    int removeAt(T index) {
+
+        Node* pTemp = new Node();
+
+        if(index == 0){
+            pTemp = head;
+            head = head->next;
+            free(pTemp);
+        }
+        else if(index == count){
+            Node* pPre = head;
+            
+            for(int i = 0; i < count - 1; i++)
+            {
+                pPre = pPre->next;
+            }
+            pPre->next = NULL;
+            tail = pPre;
+            free(pPre);
+        }
+        else{
+            Node* pTemp = head;
+            Node* pPre = head;
+
+            for(int i = 0; i < index - 1; i++)
+            {
+                pPre = pPre->next;
+            }
+            pTemp = pPre->next;
+            pPre->next = pTemp->next;
+            free(pTemp);
+        }
+
+        count--;
+    }
+
+    bool removeItem(int item) {
+
+        return false;
+    }
+
+    bool empty() {
+
+        return false;
+    }
+
+    int size() {
+
+        return count;
+    }
+    
+    int get(int index) {
+        return -1;
+    }
+
+    void set(int index, int element) {
+
+    }
+
+    int indexOf(int item) {
+        return -1;
+    }
+
+    bool contains(int item) {
+        return false;
+    }
+    
+    void clear() {
+
+    }
+    
+    string toString();
 };
